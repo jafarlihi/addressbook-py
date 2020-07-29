ae74e6eba7e42a2301693679f010703e80c4c0d6
Hikmat Jafarli <jafarlihi@gmail.com>
2020-07-29 16:37:21 +0400

    Extract token parsing logic to a decorator

--- addressbook/contact/views.py
+++ addressbook/contact/views.py
@@ -7,6 +7,7 @@ import jwt
 
 from .serializers import ContactSerializer
 from .models import Contact
+from .decorators import parse_token_user_id
 
 
 class ContactView(generics.RetrieveUpdateDestroyAPIView):
@@ -18,12 +19,9 @@ class ContactListCreateView(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
+    @parse_token_user_id
     def list(self, request, *args, **kwargs):
-        authorization = request.headers.get('Authorization')
-        token = authorization.split()[1]
-        tokenDecoded = jwt.decode(token, verify=False)
-
-        queryset = Contact.objects.filter(user__id=tokenDecoded["user_id"])
+        queryset = Contact.objects.filter(user__id=kwargs['user_id'])
 
         page = self.paginate_queryset(queryset)
         if page is not None:
@@ -33,15 +31,13 @@ class ContactListCreateView(generics.ListCreateAPIView):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
+    @parse_token_user_id
     def create(self, request, *args, **kwargs):
-        authorization = request.headers.get('Authorization')
-        token = authorization.split()[1]
-        tokenDecoded = jwt.decode(token, verify=False)
-
-        request.data["user"] = tokenDecoded["user_id"]
+        request.data["user"] = kwargs['user_id']
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
