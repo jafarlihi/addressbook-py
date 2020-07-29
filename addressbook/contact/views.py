c8c29407be0cec25d27ded2536b94dfb9ebf1d2b
Hikmat Jafarli <jafarlihi@gmail.com>
2020-07-29 18:58:30 +0400

    Check user token on all ContactView operations

--- addressbook/contact/views.py
+++ addressbook/contact/views.py
@@ -23,6 +23,30 @@ class ContactView(generics.RetrieveUpdateDestroyAPIView):
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
+    @parse_token_user_id
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        if instance.user.id != kwargs['user_id']:
+            raise PermissionDenied
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    @parse_token_user_id
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.user.id != kwargs['user_id']:
+            raise PermissionDenied
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class ContactListCreateView(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
