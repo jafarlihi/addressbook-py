5620cd246edb963a7e684a296379859aa7a915aa
Hikmat Jafarli <jafarlihi@gmail.com>
2020-07-29 13:19:17 +0400

    Let users create contact only for their own user

--- addressbook/contact/views.py
+++ addressbook/contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import status
 
 import jwt
 
@@ -21,6 +22,7 @@ class ContactListCreateView(generics.ListCreateAPIView):
         authorization = request.headers.get('Authorization')
         token = authorization.split()[1]
         tokenDecoded = jwt.decode(token, verify=False)
+
         queryset = Contact.objects.filter(user__id=tokenDecoded["user_id"])
 
         page = self.paginate_queryset(queryset)
@@ -30,3 +32,16 @@ class ContactListCreateView(generics.ListCreateAPIView):
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        authorization = request.headers.get('Authorization')
+        token = authorization.split()[1]
+        tokenDecoded = jwt.decode(token, verify=False)
+
+        request.data["user"] = tokenDecoded["user_id"]
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
